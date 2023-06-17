247df45ac4d25b673f6edc16bf87264b1474a34d
AlehandroSV <alehandrosantos99@gmail.com>
2023-06-17 10:56:35 -0300

    feat(Post): Inicializando Crud padrão com Nest CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostResolver } from './modules/post/post.resolver';
+import { PostModule } from './modules/post/post.module';
 
 @Module({
-  imports: [],
+  imports: [PostModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PostResolver],
 })
 export class AppModule {}
